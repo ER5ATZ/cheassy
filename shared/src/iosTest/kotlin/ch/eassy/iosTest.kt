e88da9fa51c7f8a3b21c351af1a8ec3ab3f6087e
Andreas Ersch <andreas.ersch@gmail.com>
2023-12-21 22:47:52 +0100

    added previously omitted file

---
+++ shared/src/iosTest/kotlin/ch/eassy/iosTest.kt
@@ -0,0 +1,12 @@
+package ch.eassy
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class IosGreetingTest {
+
+    /*@Test
+    fun testExample() {
+        assertTrue(Greeting().greet().contains("iOS"), "Check iOS is mentioned")
+    }*/
+}
